93eb84e35e4ee0a98472efdbcf293809740b0568
tomad6909 <tomad6909@wrdsb.ca>
2020-10-13 16:23:25 +0000

    finished A7Q4

--- Main.java
+++ Main.java
@@ -1,15 +1,25 @@
 /**
- *
- * @author 
+ * This program is a method named lastDigit that returns the last digit of an integer 
+ * @author David Toma
  */
 public class Main {
+
+  // create a method with variable lastDigit
+  public static int lastDigit(int integer){
+    // divide the integer by 10 to find the last integer
+    int digit = (integer % 10);
+    // return the last integer to the user
+    return digit;
+  }
 
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // input the integer that the user wants the last digit to
+    int totalLast = lastDigit(2578967);
+    System.out.println(totalLast);
     
   }
 }
